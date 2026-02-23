024a5b041bf204382e0a355256b4dc3ee00a7367
Johannes Aspman <johannes.aspman@icloud.com>
2026-02-23 14:29:43 +0100

    added menu toggle system
    Created funtions for displaying and removing both menus(local and global). Also created funtions for displaying and removing fadelayer when the menus are vissible. Menus hide when fadelayer is clicked. TODO: create buttons for enabling menus.

--- js/main.js
+++ js/main.js
@@ -6,8 +6,49 @@ function copy_element_by_id(source_id, destination_id){
     destination.innerHTML = source.innerHTML;
 }
 
+function enable_fade_layer(){
+    let fade_layer = document.querySelector(".fade-layer");
+
+    fade_layer.classList.remove("hidden");
+}
+function disable_fade_layer(){
+    let fade_layer = document.querySelector(".fade-layer");
+
+    fade_layer.classList.add("hidden");
+}
+
+
+function show_mobile_nav(){
+    hide_menus();
+    enable_fade_layer();
+    let menu = document.querySelector("nav#mobile-global-nav");
+
+    menu.classList.add("show-menu");
+}
+
+
+function show_mobile_aside(){
+    hide_menus();
+    enable_fade_layer();
+
+    let menu = document.querySelector("aside#mobile-local-nav");
+
+    menu.classList.add("show-menu");
+}
+
+
+function hide_menus(){
+    disable_fade_layer();
+    let global_menu = document.querySelector("nav#mobile-global-nav");
+    let local_menu = document.querySelector("aside#mobile-local-nav");
+
+    global_menu.classList.remove("show-menu");
+    local_menu.classList.remove("show-menu");
+}
 
 copy_element_by_id("global-nav", "mobile-global-nav");
 copy_element_by_id("local-nav", "mobile-local-nav");
 
 
+let fade_layer = document.querySelector(".fade-layer");
+fade_layer.addEventListener("click", hide_menus);
